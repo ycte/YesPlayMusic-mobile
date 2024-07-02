d6e80bb24ee6de3ac4341fde269ad5aae8778f69
ycte <b15260056838@163.com>
2024-07-02 21:52:12 +0800

    chore(): eslint --fix

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import Greet from "./components/Greet.vue";
+import Greet from './components/Greet.vue'
 </script>
 
 <template>
@@ -10,13 +10,13 @@ import Greet from "./components/Greet.vue";
 
     <div class="row">
       <a href="https://vitejs.dev" target="_blank">
-        <img src="/vite.svg" class="logo vite" alt="Vite logo" />
+        <img src="/vite.svg" class="logo vite" alt="Vite logo">
       </a>
       <a href="https://tauri.app" target="_blank">
-        <img src="/tauri.svg" class="logo tauri" alt="Tauri logo" />
+        <img src="/tauri.svg" class="logo tauri" alt="Tauri logo">
       </a>
       <a href="https://vuejs.org/" target="_blank">
-        <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+        <img src="./assets/vue.svg" class="logo vue" alt="Vue logo">
       </a>
     </div>
 
@@ -144,5 +144,4 @@ button {
     background-color: #0f0f0f69;
   }
 }
-
 </style>
