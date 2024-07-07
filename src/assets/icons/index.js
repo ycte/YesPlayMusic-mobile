cf5cb940dec99af11e57597b423933af398bc316
ycte <b15260056838@163.com>
2024-07-07 17:01:53 +0800

    chore(assets): static resource

---
+++ src/assets/icons/index.js
@@ -0,0 +1,9 @@
+// function importAll(requireContext) {
+//   for (const key of requireContext.keys()) {
+//     requireContext(key)
+//   }
+// }
+
+const req = import.meta.glob('./*.svg')
+// console.log(req)
+// importAll(req)
