7837bcff8ce42369ac175a59f0f219e1a74fc2f1
ycte <b15260056838@163.com>
2024-07-08 11:14:55 +0800

    chore(header): linear-gradient

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -21,15 +21,16 @@ const headerTitle = computed(() => {
 
 <style lang="less">
   .blur-header {
-    background-color: rgba(255, 255, 255, 0.5);
-    backdrop-filter: blur(10px);
+    background: linear-gradient(to bottom, rgba(255, 255, 255, 1), rgba(255, 255, 255, 0.0));
+    // background-color: rgba(255, 255, 255, 0.5);
+    // backdrop-filter: blur(10px);
     padding: 10px;
     position: fixed;
     top: 0;
     left: 0;
     right: 0;
     z-index: 256;
-    border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+    // border-bottom: 1px solid rgba(0, 0, 0, 0.1);
 
     &.pc-header {
       height: 49px;
